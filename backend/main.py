c94b9f4cce924eef180f9975c2651b7ba5136d33
Roman Zotsenko <romazotsenko15@gmail.com>
2025-04-16 11:05:03 +0200

    Add CORS middleware and axios; remove unused TypeScript shim

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,30 @@
 import fastapi as fa
+from fastapi.middleware.cors import CORSMiddleware
 from router import ALL_CONTROLLERS
 
 API = fa.FastAPI(title="API", version="0.1.0", root_path="/api")
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+API.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # TODO: z env
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+for router in ALL_CONTROLLERS:
+    API.include_router(router)
 
 @API.get("/")
 async def root():
     return {"message": "API is working"}
 
-for router in ALL_CONTROLLERS:
-    API.include_router(router)
+
+
